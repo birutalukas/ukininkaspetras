75e77e3b0c27e27ea68487cdee0d8892c2a6b4a9
birutalukas <biruta.lukas@gmail.com>
2025-04-27 12:18:30 +0300

    Add Google Tag Manager

--- wp-content/themes/sage/index.php
+++ wp-content/themes/sage/index.php
@@ -7,6 +7,14 @@
 
     <?php if (defined('WP_ENV') && WP_ENV == 'production') : ?>
 
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-P9DPS7H6');</script>
+      <!-- End Google Tag Manager -->
+
       <!-- Google tag (gtag.js) -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-YWKXFW9VEC"></script>
       <script>
@@ -22,6 +30,14 @@
   </head>
 
   <body <?php body_class(); ?>>
+
+    <?php if (defined('WP_ENV') && WP_ENV == 'production') : ?>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9DPS7H6"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+    <?php endif; ?>
+
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
